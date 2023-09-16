1d3c3c6d11bed2712137d5b5db9adcb23aa516db
Karlushyna Lesya <karlushyna.l@gmail.com>
2023-09-15 20:01:41 -0400

    upd

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './About.module.css';
 
 const About = () => {
   return (
     <div>
       <h1>About</h1>
       <div className="container">Hello Hello</div>
+      <button className={styles.button}>
       <Link to="/page">PUSH</Link>
+      </button>
     </div>
   );
 };
